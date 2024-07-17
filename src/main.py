68f368200f4498d592c6897c4c7fa9e98922b5f1
Kaushik Roy <kauroy1994@gmail.com>
2024-07-17 14:11:28 +0000

    updated files

--- src/main.py
+++ src/main.py
@@ -18,24 +18,40 @@ class MTSS_Copilot:
         return random_user_role, random_user_query
 
     @staticmethod
-    def run_demo():
+    def simulate_QA_agent_turn(user_role, user_query, data):
+
+        response = None
+        system_template = AssetLoader.get_templates()[user_role]
+        return system_template, response
+
+    @staticmethod
+    def run_demo(turns = 2):
         
-        #format and organize data -- grounding/alignment w customization
         mtss_text_data = AssetLoader.read_data()
         mtss_data_repr = Knowledge_Representation.organize_data(mtss_text_data)
-        user_role, user_query = MTSS_Copilot.simulate_user_turn()
-        print (user_role, user_query)
-        #-- alignment/instructability w customization
+
+        for _ in range(turns):
+            user_role, user_query = MTSS_Copilot.simulate_user_turn()
+
+            print ('\n ===== USER attributes =====\n')
+            print ('user role:', user_role)
+            print ('user_query', user_query)
+            
+            system_template, response = MTSS_Copilot.simulate_QA_agent_turn(user_role, user_query, mtss_data_repr)
+            print ('\n ===== SYSTEM INSTRUCTIONS ===== \n',system_template)
 
 
 if __name__ == '__main__':
     try:
         MTSS_Copilot.run_demo()
         os.system('rm -rf src/assets/__pycache__')
+        os.system('rm -rf src/copilots/__pycache__')
 
     except RuntimeError as e:
         print (e)
         os.system('rm -rf src/assets/__pycache__')
+        os.system('rm -rf src/copilots/__pycache__')
 
     except KeyboardInterrupt:
         os.system('rm -rf src/assets/__pycache__')
+        os.system('rm -rf src/copilots/__pycache__')
